3242c6d70272a7405481de6e8f01fec1a6b50d0d
JYY <jiang.yu.yan@icloud.com>
2021-04-27 15:17:50 +0800

    Solve cache lab.

--- cache/csim.c
+++ cache/csim.c
@@ -1,7 +1,134 @@
 #include "cachelab.h"
-
-int main()
+#include <getopt.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+struct Line {
+  int valid;
+  long tag;
+};
+int hexcharToInt(char c) {
+  if (c >= '0' && c <= '9') return c - '0';
+  if (c >= 'a' && c <= 'z') return c - 'a' + 10;
+  if (c >= 'A' && c <= 'Z') return c - 'A' + 10;
+  return 0;
+}
+int main(int argc, char *argv[])
 {
-    printSummary(0, 0, 0);
-    return 0;
+  int opt, s = 0, E = 0, b = 0;
+  bool verbose = false;
+  FILE* fileptr;
+  while ((opt = getopt(argc, argv, "hvs:E:b:t:")) != -1) {
+    switch (opt) {
+      case 'h':
+        break;
+      case 'v':
+        verbose = true;
+        break;
+      case 's':
+        s = atoi(optarg);
+        break;
+      case 'E':
+        E = atoi(optarg);
+        break;
+      case 'b':
+        b = atoi(optarg);
+        break;
+      case 't':
+        if ((fileptr = fopen(optarg, "r")) == NULL) {
+          printf("No such file.\n");
+          exit(1);
+        }
+        break;
+    }
+  }
+  
+  if (s == 0 || E == 0 || b == 0) {
+    printf("please input option s, E and b.\n");
+    exit(2);
+  }
+  int S = 1 << s;
+  struct Line *cache;
+  if ((cache = (struct Line *)malloc((S * E) * sizeof(struct Line))) == NULL)
+    printf("Failed to allocate memory for cache.\n");
+  for (int i = 0; i < S * E; ++i) {
+    cache[i].valid = 0;
+  }
+  int hit_count = 0, miss_count = 0, eviction_count = 0;
+  char line[100];
+  char instruction;
+  long address;
+  while (fgets(line, 100, fileptr)) {
+    instruction = line[1];
+    if (instruction == ' ') continue;
+    address = 0;
+    char curr_c = line[3];
+    int id = 3;
+    for (; curr_c != ','; curr_c = line[++id]) {
+      address = address * 16 + hexcharToInt(curr_c);
+    }
+    for (; curr_c != '\n'; curr_c = line[++id]) {
+      // find \n;
+    }
+    line[id] = '\0';
+    printf("%s", line+1);
+    address = address >> b;
+    long tag = address >> s;
+    int set = address ^ (tag << s);
+    int tail_id = (set + 1) * E;
+    int min_valid = E+1, min_valid_id = set * E, max_valid = 0;
+    bool isHit = false;
+    int hitId = 0;
+    for (int i = set * E; i < tail_id; ++i) {
+      int curr_valid = cache[i].valid;
+      if (curr_valid < min_valid) {
+        min_valid = curr_valid;
+        min_valid_id = i;
+      }
+      if (curr_valid) {
+        max_valid = max_valid > curr_valid ? max_valid : curr_valid;
+        if (cache[i].tag == tag) {
+          ++hit_count;
+          if (verbose) printf(" hit");
+          isHit = true;
+          hitId = i;
+        }
+      }
+    }
+    if (!isHit) {
+      ++miss_count;
+      if (verbose) printf(" miss");
+      if (min_valid) {
+        ++eviction_count;
+        if (verbose) printf(" evicion");
+        for (int i = set * E; i < tail_id; ++i) {
+          if (i != min_valid_id) {
+            --(cache[i].valid);
+          }
+        }
+        cache[min_valid_id].valid = E;
+      } else {
+        cache[min_valid_id].valid = max_valid + 1;
+      }
+      cache[min_valid_id].tag = tag;
+    } else {
+      int hit_valid = cache[hitId].valid;
+      for (int i = set * E; i < tail_id; ++i) {
+        if (cache[i].valid > hit_valid) {
+          --(cache[i].valid);
+        }
+      }
+      cache[hitId].valid = max_valid;
+    }
+    if (instruction == 'M') {
+      ++hit_count;
+      if (verbose) printf(" hit");
+    }
+    printf("\n");
+  }
+
+  fclose(fileptr);
+  printSummary(hit_count, miss_count, eviction_count);
+  return 0;
 }
--- cache/trans.c
+++ cache/trans.c
@@ -22,6 +22,292 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[N][M], int B[M][N])
 {
+  int i, j, row, col, x0, x1, x2, x3, x4, x5, x6, x7;
+  switch (M) {
+    case 32:
+      for (i = 0; i < 4; ++i) {
+        for (j = 0; j < 4; ++j) {
+          if (i == j) {
+            for (row = i*8; row < (i+1)*8; ++row) {
+              col = j*8;
+              x0 = A[row][col];
+              x1 = A[row][col+1];
+              x2 = A[row][col+2];
+              x3 = A[row][col+3];
+              x4 = A[row][col+4];
+              x5 = A[row][col+5];
+              x6 = A[row][col+6];
+              x7 = A[row][col+7];
+              B[row][col] = x0;
+              B[row][col+1] = x1;
+              B[row][col+2] = x2;
+              B[row][col+3] = x3;
+              B[row][col+4] = x4;
+              B[row][col+5] = x5;
+              B[row][col+6] = x6;
+              B[row][col+7] = x7;
+            }
+            for (row = 0; row < 8; ++row) {
+              for (col = row+1; col < 8; ++col) {
+                x0 = B[i*8+row][j*8+col];
+                B[i*8+row][j*8+col] = B[j*8+col][i*8+row];
+                B[j*8+col][i*8+row] = x0;
+              }
+            }
+          } else {
+            for (row = i*8; row < (i+1)*8; ++row) {
+              for (col = j*8; col < (j+1)*8; ++col) {
+                B[row][col] = A[col][row];
+              }
+            }
+          }
+          
+        }
+      }
+      // for (i = 0; i < 4; ++i) {
+      //   for (j = 0; j < 4; ++j) {
+      //     for (row = 0; row < 8; ++row) {
+      //       x0 = A[j*8][i*8+row];
+      //       x1 = A[j*8+1][i*8+row];
+      //       x2 = A[j*8+2][i*8+row];
+      //       x3 = A[j*8+3][i*8+row];
+      //       x4 = A[j*8+4][i*8+row];
+      //       x5 = A[j*8+5][i*8+row];
+      //       x6 = A[j*8+6][i*8+row];
+      //       x7 = A[j*8+7][i*8+row];
+      //       B[i*8+row][j*8] = x0;
+      //       B[i*8+row][j*8+1] = x1;
+      //       B[i*8+row][j*8+2] = x2;
+      //       B[i*8+row][j*8+3] = x3;
+      //       B[i*8+row][j*8+4] = x4;
+      //       B[i*8+row][j*8+5] = x5;
+      //       B[i*8+row][j*8+6] = x6;
+      //       B[i*8+row][j*8+7] = x7;
+      //     }
+      //   }
+      // }
+      break;
+    case 64:
+      for (i = 0; i < 8; ++i) {
+        for (j = 0; j < 8; ++j) {
+          if (i*8+j >= 61) continue;
+          x0 = (i*8 + j + 1) / 8; // [id+1]row
+          x1 = (j + 1) % 8; // [id+1]col
+          x2 = (i*8 + j + 2) / 8; // [id+2]row
+          x3 = (j + 2) % 8; // [id+2]col
+          if (i == j) { // use cell[id+1] and cell[id+2]
+            for (row = 0; row < 4; ++row) {
+              for (col = 0; col < 8; ++col) {
+                B[x0*8+row][x1*8+col] = A[j*8+row][i*8+col];
+              }
+            }
+            for (row = 4; row < 8; ++row) {
+              for (col = 0; col < 8; ++col) {
+                B[x2*8+row-4][x3*8+col] = A[j*8+row][i*8+col];
+              }
+            }
+            for (row = 0; row < 4; ++row) {
+              for (col = 0; col < 4; ++col) {
+                B[i*8+row][j*8+col] = B[x0*8+col][x1*8+row];
+              }
+              for (col = 4; col < 8; ++col) {
+                B[i*8+row][j*8+col] = B[x2*8+col-4][x3*8+row];
+              }
+            }
+            for (row = 4; row < 8; ++row) {
+              for (col = 0; col < 4; ++col) {
+                B[i*8+row][j*8+col] = B[x0*8+col][x1*8+row];
+              }
+              for (col = 4; col < 8; ++col) {
+                B[i*8+row][j*8+col] = B[x2*8+col-4][x3*8+row];
+              }
+            }
+          } else { // just use cell[id] when i != j
+            for (row = 0; row < 4; ++row) {
+              for (col = 0; col < 4; ++col) {
+                B[i*8+row][j*8+col] = A[j*8+col][i*8+row];
+              }
+            } // A left-up reverse to B left-up
+            for (row = 0; row < 4; ++row) {
+              for (col = 4; col < 8; ++col) {
+                B[i*8+row][j*8+col] = A[j*8+col-4][i*8+row+4];
+              }
+            } // A right-up reverse to B right-up 
+            for (row = 4; row < 8; ++row) {
+              // save B right-up
+              x0 = B[i*8+row-4][j*8+4];
+              x1 = B[i*8+row-4][j*8+5];
+              x2 = B[i*8+row-4][j*8+6];
+              x3 = B[i*8+row-4][j*8+7];
+                
+              // A left-bottom reverse to B right-up
+              B[i*8+row-4][j*8+4] = A[j*8+4][i*8+row-4];
+              B[i*8+row-4][j*8+5] = A[j*8+5][i*8+row-4];
+              B[i*8+row-4][j*8+6] = A[j*8+6][i*8+row-4];
+              B[i*8+row-4][j*8+7] = A[j*8+7][i*8+row-4]; 
+
+              // saved B right-up to B left-bottom
+              B[i*8+row][j*8] = x0;
+              B[i*8+row][j*8+1] = x1;
+              B[i*8+row][j*8+2] = x2;
+              B[i*8+row][j*8+3] = x3;
+            } // A left-bottom reverse to B right-up; saved B right-up move to B left-bottom
+            for (row = 4; row < 8; ++row) {
+              for (col = 4; col < 8; ++col) {
+                B[i*8+row][j*8+col] = A[j*8+col][i*8+row];
+              }
+            } // A right-bottom reverse to B right-bottom;
+            
+          }
+          
+        }
+      }
+      // deal with cell 63
+      i = 7;
+      j = 7;
+      x0 = 7; // [id+1]row
+      x1 = 5; // [id+1]col
+      x2 = 7; // [id+2]row
+      x3 = 6; // [id+2]col
+      for (row = 0; row < 4; ++row) {
+        for (col = 0; col < 8; ++col) {
+          B[x0*8+row][x1*8+col] = A[j*8+row][i*8+col];
+        }
+      }
+      for (row = 4; row < 8; ++row) {
+        for (col = 0; col < 8; ++col) {
+          B[x2*8+row-4][x3*8+col] = A[j*8+row][i*8+col];
+        }
+      }
+      for (row = 0; row < 4; ++row) {
+        for (col = 0; col < 4; ++col) {
+          B[i*8+row][j*8+col] = B[x0*8+col][x1*8+row];
+        }
+        for (col = 4; col < 8; ++col) {
+          B[i*8+row][j*8+col] = B[x2*8+col-4][x3*8+row];
+        }
+      }
+      for (row = 4; row < 8; ++row) {
+        for (col = 0; col < 4; ++col) {
+          B[i*8+row][j*8+col] = B[x0*8+col][x1*8+row];
+        }
+        for (col = 4; col < 8; ++col) {
+          B[i*8+row][j*8+col] = B[x2*8+col-4][x3*8+row];
+        }
+      }
+      // deal with cell 61 and 62
+      i = 7;
+      for (j = 5; j < 7; ++j) {
+        for (row = 0; row < 4; ++row) {
+          for (col = 0; col < 4; ++col) {
+            B[i*8+row][j*8+col] = A[j*8+col][i*8+row];
+          }
+        } // A left-up reverse to B left-up
+        for (row = 0; row < 4; ++row) {
+          for (col = 4; col < 8; ++col) {
+            B[i*8+row][j*8+col] = A[j*8+col-4][i*8+row+4];
+          }
+        } // A right-up reverse to B right-up 
+        for (row = 4; row < 8; ++row) {
+          // save B right-up
+          x0 = B[i*8+row-4][j*8+4];
+          x1 = B[i*8+row-4][j*8+5];
+          x2 = B[i*8+row-4][j*8+6];
+          x3 = B[i*8+row-4][j*8+7];
+            
+          // A left-bottom reverse to B right-up
+          B[i*8+row-4][j*8+4] = A[j*8+4][i*8+row-4];
+          B[i*8+row-4][j*8+5] = A[j*8+5][i*8+row-4];
+          B[i*8+row-4][j*8+6] = A[j*8+6][i*8+row-4];
+          B[i*8+row-4][j*8+7] = A[j*8+7][i*8+row-4]; 
+
+          // saved B right-up to B left-bottom
+          B[i*8+row][j*8] = x0;
+          B[i*8+row][j*8+1] = x1;
+          B[i*8+row][j*8+2] = x2;
+          B[i*8+row][j*8+3] = x3;
+        } // A left-bottom reverse to B right-up; saved B right-up move to B left-bottom
+        for (row = 4; row < 8; ++row) {
+          for (col = 4; col < 8; ++col) {
+            B[i*8+row][j*8+col] = A[j*8+col][i*8+row];
+          }
+        } // A right-bottom reverse to B right-bottom;
+      }
+      break;
+    case 61:
+      // bottom row
+      i = 7;
+      for (col = 0; col < 64; ++col) {
+        for (row = i*8; row < 61; ++row) {
+          B[row][col] = A[col][row];
+        }
+      }
+      // for (j = 0; j < 8; ++j) {
+      //   for (row = 0; row < 5; ++row) {
+      //     x0 = A[j*8][i*8+row];
+      //     x1 = A[j*8+1][i*8+row];
+      //     x2 = A[j*8+2][i*8+row];
+      //     x3 = A[j*8+3][i*8+row];
+      //     x4 = A[j*8+4][i*8+row];
+      //     x5 = A[j*8+5][i*8+row];
+      //     x6 = A[j*8+6][i*8+row];
+      //     x7 = A[j*8+7][i*8+row];
+      //     B[i*8+row][j*8] = x0;
+      //     B[i*8+row][j*8+1] = x1;
+      //     B[i*8+row][j*8+2] = x2;
+      //     B[i*8+row][j*8+3] = x3;
+      //     B[i*8+row][j*8+4] = x4;
+      //     B[i*8+row][j*8+5] = x5;
+      //     B[i*8+row][j*8+6] = x6;
+      //     B[i*8+row][j*8+7] = x7;
+      //   }
+      // }
+      // right col
+      j = 8;
+      for (row = 0; row < 56; ++row) {
+        for (col = 8*j; col < 67; ++col) {
+          B[row][col] = A[col][row];
+        }
+      }
+      // right-bottom
+      i = 7; j = 8;
+      for (row = i*8; row < 61; ++row) {
+        for (col = j*8; col < 67; ++col) {
+          B[row][col] = A[col][row];
+        }
+      }
+      // for (row = 0; row < 5; ++row) {
+      //   x0 = A[j*8][i*8+row];
+      //   x1 = A[j*8+1][i*8+row];
+      //   x2 = A[j*8+2][i*8+row];
+      //   B[i*8+row][j*8] = x0;
+      //   B[i*8+row][j*8+1] = x1;
+      //   B[i*8+row][j*8+2] = x2;
+      // }
+      // others
+      for (i = 0; i < 7; ++i) {
+        for (j = 0; j < 8; ++j) {
+          for (col = 0; col < 8; ++col) {
+            x0 = A[j*8+col][i*8];
+            x1 = A[j*8+col][i*8+1];
+            x2 = A[j*8+col][i*8+2];
+            x3 = A[j*8+col][i*8+3];
+            x4 = A[j*8+col][i*8+4];
+            x5 = A[j*8+col][i*8+5];
+            x6 = A[j*8+col][i*8+6];
+            x7 = A[j*8+col][i*8+7];
+            B[i*8][j*8+col] = x0;
+            B[i*8+1][j*8+col] = x1;
+            B[i*8+2][j*8+col] = x2;
+            B[i*8+3][j*8+col] = x3;
+            B[i*8+4][j*8+col] = x4;
+            B[i*8+5][j*8+col] = x5;
+            B[i*8+6][j*8+col] = x6;
+            B[i*8+7][j*8+col] = x7;
+          }
+        }
+      }
+  } 
 }
 
 /* 
